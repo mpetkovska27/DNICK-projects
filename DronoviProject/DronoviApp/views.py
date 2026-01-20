1d3d19cea0ed8a0c2422a3903062dc3ae4a270da
Martina <martapetkovska@gmail.com>
2026-01-20 22:34:47 +0100

    Dronovi

--- DronoviProject/DronoviApp/views.py
+++ DronoviProject/DronoviApp/views.py
@@ -4,64 +4,28 @@ from .forms import *
 # Create your views here.
 
 def index(request):
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def index(request): # почетна страна - листа на дронови
-    # Земаме уникатни типови на дронови
     tipovi = Dron.objects.values_list('tip', flat=True).distinct()
+    grupirani_tipovi = {}
 
-    # Групираме во речник { 'Тип1': [дрон1, dрон2], 'Тип2': [дрон3] }
-    grupirani_dronovi = {}
-    for t in tipovi:
-        grupirani_dronovi[t] = Dron.objects.filter(tip=t)
+    for tip in tipovi:
+        grupirani_tipovi[tip] = Dron.objects.filter(tip = tip)
+    return render(request, 'index.html', {'grupirani_tipovi': grupirani_tipovi})
 
-    return render(request, 'index.html', {'grupirani_dronovi': grupirani_dronovi})
+def dron_details(request, dron_id):
+    dron = get_object_or_404(Dron, pk=dron_id)
 
-def dron_details(request, dron_id): #страна за детали на дрон по id
-    dron = get_object_or_404(Dron, id=dron_id)
-    # Ги прикажуваме сите резервации за овој дрон
-    rezervacii = Rezervacija.objects.filter(dron=dron)
+    rezervacii = Rezervacija.objects.filter(dron = dron).all()
 
-    return render(request, 'dron_details.html', {'dron': dron,'rezervacii': rezervacii})
+    return render(request, 'dron_details.html', {'dron':dron, 'rezervacii':rezervacii})
 
 def add_rezervacija(request):
     if request.method == 'POST':
         form = RezervacijaForm(request.POST)
         if form.is_valid():
             rezervacija = form.save(commit=False)
-            # Автоматски го доделуваме пилотот (најавениот корисник)
-            rezervacija.odgovoren = Pilot.objects.get(user=request.user)
+            rezervacija.odgovoren = Pilot.objects.filter(user=request.user)
             rezervacija.save()
             return redirect('index')
     else:
         form = RezervacijaForm()
-    return render(request, 'form.html', {'form': form})
+    return render(request, 'form.html', {'form':form})
